e773200dd1332022846bda266e493f46311c83d9
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2024-09-14 16:57:44 -0300

    adicionei mais coisas

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:app_humor/screens/RegisterScreen.dart';
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
@@ -11,11 +12,26 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      // drawer: Drawer(),
       appBar: AppBar(
         title: Text('Diário do humor'),
         backgroundColor: Colors.blue[50],
       ),
+      body: Container(
+        child: Center(
+          child: Text('Bem-vindo ao Diário do Humor!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Registerscreen()));
+        }, 
+        child: Icon(Icons.add,color: Colors.black,),
+        
+        backgroundColor: Colors.blue[50],
+        
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
---
+++ lib/screens/RegisterScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Registerscreen extends StatefulWidget {
+  const Registerscreen({super.key});
+
+  @override
+  State<Registerscreen> createState() => _RegisterscreenState();
+}
+
+class _RegisterscreenState extends State<Registerscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
